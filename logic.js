0a8ece7685cb308a7977ced3b17a96b8bcf92300
cicadav <pathfinder10.vox@gmail.com>
2019-07-21 20:10:36 +0900

    より細かくファイル分割 objectクラス、characterクラス(extend object)を作成 main.js/objs連想配列で背景、キャラクター(社畜ちゃん)をnew gameLogic.jsやrender.jsでobjsを参照

--- logic.js
+++ logic.js
@@ -1,11 +1,5 @@
 // ゲームロジック
-
-var key_buff = new Array(256);  // キー押下状態の維持用
-var KEY_CODE_W = 87;
-var KEY_CODE_A = 65;
-var KEY_CODE_S = 83;
-var KEY_CODE_D = 68;
-var KEY_CODE_SPACE = 32;
+let key_buff = new Array(256);  // キー押下状態の維持用
 
 // キー押下時の処理
 function keyDownHandler(e) {
@@ -14,14 +8,14 @@ function keyDownHandler(e) {
     // 左方向の初速を設定
     switch(e.keyCode) {
         case KEY_CODE_A:
-            shachikuXSpeed = -300;
+            objs[NAME_SHACHIKU].xAccel = -300;
             break;
         case KEY_CODE_D:
-            shachikuXSpeed = 300;
+            objs[NAME_SHACHIKU].xAccel = 300;
             break;
         case KEY_CODE_SPACE:
-            shachikuYSpeed = 42;
-            jumping = true;
+            objs[NAME_SHACHIKU].yAccel = 42;
+            objs[NAME_SHACHIKU].jumping = true;
         default:
             break;
     }
@@ -40,14 +34,43 @@ function actionKeeper() {
         return;
     }
     if(key_buff[KEY_CODE_A]) {
-        shachikuXSpeed = -300;
+        objs[NAME_SHACHIKU].xAccel = -300;
         return;
     }
     if(key_buff[KEY_CODE_D]) {
-        shachikuXSpeed = 300;
+        objs[NAME_SHACHIKU].xAccel = 300;
         return;
     }
     
     // キー解放状態の場合
-    shachikuXSpeed = 0;
+    objs[NAME_SHACHIKU].xAccel = 0;
+}
+
+function gameProcess(timestamp) {
+    // 時差取得
+    var delta = 0;
+    if(lastTimestamp != null) {
+        delta = (timestamp - lastTimestamp) / 1000;
+    }
+    lastTimestamp = timestamp;
+
+    // キャラクタ座標計算
+    actionKeeper();
+    if(objs[NAME_SHACHIKU].jumping) {
+        objs[NAME_SHACHIKU].jumpTime += delta * 16;
+    }
+    objs[NAME_SHACHIKU].x = objs[NAME_SHACHIKU].x + objs[NAME_SHACHIKU].xAccel * delta;
+    objs[NAME_SHACHIKU].y = (0.5 * GRAVITY * objs[NAME_SHACHIKU].jumpTime * objs[NAME_SHACHIKU].jumpTime - objs[NAME_SHACHIKU].yAccel * objs[NAME_SHACHIKU].jumpTime) + (SCREEN_HEIGHT - objs[NAME_SHACHIKU].h);
+
+    // 当たり判定
+    if(objs[NAME_SHACHIKU].x > SCREEN_WIDTH - objs[NAME_SHACHIKU].w) {
+        objs[NAME_SHACHIKU].x = SCREEN_WIDTH - objs[NAME_SHACHIKU].w;
+    }else if(objs[NAME_SHACHIKU].x < 0) {
+        objs[NAME_SHACHIKU].x = 0;
+    }else if(objs[NAME_SHACHIKU].y > SCREEN_HEIGHT - objs[NAME_SHACHIKU].h) {
+        objs[NAME_SHACHIKU].y = SCREEN_HEIGHT - objs[NAME_SHACHIKU].h;
+        objs[NAME_SHACHIKU].yAccel = 0;
+        objs[NAME_SHACHIKU].jumpTime = 0;
+        objs[NAME_SHACHIKU].jumping = false;
+    }
 }
--- main.js
+++ main.js
@@ -1,15 +1,9 @@
-var SCREEN_WIDTH = 800;
-var SCREEN_HEIGHT = 600;
-
 window.addEventListener('load', init);
 window.addEventListener('keydown', keyDownHandler);
 window.addEventListener('keyup', keyUpHandler);
 
-
-// init update render
-
-var canvas;
-var ctx;
+let canvas;
+let ctx;
 
 function init() {
     canvas = document.getElementById('maincanvas');
@@ -23,94 +17,18 @@ function init() {
     });
 }
 
-var lastTimestamp = null;
-var gravity = 9.8;
-var shachikuW = 100;
-var shachikuH = 140;
-var shachikuX = 0;
-var shachikuY = SCREEN_HEIGHT - shachikuH;
-var shachikuXSpeed = 0;
-var shachikuYSpeed = 0;
-var jumpTime = 0;
-var jumping = false;
+let lastTimestamp;
+let objs = {
+    [NAME_BACK]: new object(SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0),
+    [NAME_SHACHIKU]: new character(100, 140, 0, 460)
+};
 
 function update(timestamp) {
-    // 時差取得
-    var delta = 0;
-    if(lastTimestamp != null) {
-        delta = (timestamp - lastTimestamp) / 1000;
-    }
-    lastTimestamp = timestamp;
-
-    /* 分割予定-> */
-    // キャラクタ座標計算
-    actionKeeper();
-    if(jumping) {
-        jumpTime += delta * 16;
-    }
-    shachikuX = shachikuX + shachikuXSpeed * delta;
-    shachikuY = (0.5 * gravity * jumpTime * jumpTime - shachikuYSpeed * jumpTime) + (canvas.height - shachikuH);
-
-    // 当たり判定
-    if(shachikuX > canvas.width - shachikuW) {
-        shachikuX = canvas.width - shachikuW;
-    }else if(shachikuX < 0) {
-        shachikuX = 0;
-    }else if(shachikuY > canvas.height - shachikuH) {
-        shachikuY = canvas.height - shachikuH;
-        shachikuYSpeed = 0;
-        jumpTime = 0;
-        jumping = false;
-    }
-    /* <-分割予定 */
+    // game処理実行
+    gameProcess(timestamp);
 
     requestAnimationFrame(update);
 
     // 描画
     render();
 }
-
-function render() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    ctx.drawImage(Asset.images['back'], 0, 0);
-    ctx.drawImage(Asset.images['shachiku'], shachikuX, shachikuY);
-}
-
-
-// グラフィックアセット読み込み周り
-
-var Asset = {};
-
-Asset.assets = [
-    { type: 'image', name: 'back', src: 'assets/back.png' },
-    { type: 'image', name: 'shachiku', src: 'assets/shachiku.png' }
-];
-
-Asset.images = {};
-
-Asset.loadAssets = function(onComplete) {
-    var total = Asset.assets.length;
-    var loadCount = 0;
-    var onLoad = function() {
-        loadCount++;
-        if (loadCount >= total) {
-            onComplete();
-        }
-    };
-
-    Asset.assets.forEach(function(asset) {
-        switch (asset.type) {
-            case 'image':
-                Asset._loadImage(asset, onLoad);
-                break;
-        }
-    });
-};
-
-Asset._loadImage = function(asset, onLoad) {
-    var image = new Image();
-    image.src = asset.src;
-    image.onload = onLoad;
-    Asset.images[asset.name] = image;
-};
